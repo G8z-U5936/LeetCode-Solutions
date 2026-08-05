d90e24321dade26cba1a658bc52b951c8ae5ff5b
G8z-U5936 <258809380+G8z-U5936@users.noreply.github.com>
2026-08-05 19:57:46 +0530

    Time: 0 ms (100%), Space: 19.5 MB (23.77%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -0,0 +1,87 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    # 1st approach can be reversing the list and removing the the element and then finally rrversing the linked list 
+    # overall t.c --- o(n)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2nd approach using  two pointer:
+        dummy = ListNode(0,head)
+        first = dummy 
+        second = dummy
+
+        for _ in range(n+1):
+            first = first.next
+        
+        while first:
+            first = first.next
+            second = second.next
+        
+        second.next = second.next.next
+        
+        return dummy.next
+        # t.c ------ o(L)
+        # s.c ---------- o(1) -- no extra space used
+        
+# 3rd approach --- using only n<n and then temp.next = temp.next.next. only single pointer is enough for this approach:
+#  n+1 -- only works in two pointer to reach the n-1th node.
+
+        lenght = head
+        temp = head
+        while temp:
+            lenght += 1
+            temp = temp.next
+            
+        if n == lenth:
+            return head.next
+        
+        temp = head
+        for _ in range(length-n-1):
+            temp = temp.next
+
+        temp.next = temp.next.next
+
+        return head
+
+
+
+
+
+
+
+
+
+
+
+
