0207a8fd08d3859a511060a0eedf9d41fbaff872
G8z-U5936 <258809380+G8z-U5936@users.noreply.github.com>
2026-08-05 19:31:13 +0530

    Time: 231 ms (87.88%), Space: 144.8 MB (36.36%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,29 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        from typing import List
+
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        # Build adjacency list
+        graph = [[] for _ in range(n)]
+        for u, v in invocations:
+            graph[u].append(v)
+
+        # Mark suspicious methods using DFS
+        suspicious = [False] * n
+
+        def dfs(node):
+            suspicious[node] = True
+            for nei in graph[node]:
+                if not suspicious[nei]:
+                    dfs(nei)
+
+        dfs(k)
+
+        # Check if any non-suspicious method calls a suspicious one
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+
+        # Return remaining methods
+        return [i for i in range(n) if not suspicious[i]]
