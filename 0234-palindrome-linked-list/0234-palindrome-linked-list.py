9b68d9cef3a5ffa08a3ba1c45014aec609a0f5ff
Divyanka Gupta <258809380+G8z-U5936@users.noreply.github.com>
2026-08-27 18:39:34 +0530

    Time: 13 ms (95.89%), Space: 53.6 MB (25.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -19,7 +19,7 @@ class Solution:
         n = len(lst)
         i = 0
         j = n - 1
-        while i < j:
+        while i <= j:
             if lst[i] == lst[j]:
                 i += 1
                 j -= 1
