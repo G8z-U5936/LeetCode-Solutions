363b458a30e007031c4798e5b074bce69629d634
Divyanka Gupta <258809380+G8z-U5936@users.noreply.github.com>
2026-09-04 13:00:25 +0530

    Time: 3 ms (50.16%), Space: 20.5 MB (38.95%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -26,3 +26,44 @@ class Solution:
         return height(root) != -1 
 
 # --------------------iterative postorder solution----------------------
+            
+        if not root:
+            return True
+
+        stack = [(root, False)]
+        heights = {}
+
+        while stack:
+            node, visited = stack.pop()
+
+            if not node:
+                continue
+
+            if visited:
+                left_height = heights.get(node.left, 0)
+                right_height = heights.get(node.right, 0)
+
+                if abs(left_height - right_height) > 1:
+                    return False
+
+                heights[node] = 1 + max(left_height, right_height)
+
+            else:
+                stack.append((node, True))
+                stack.append((node.right, False))
+                stack.append((node.left, False))
+
+        return True        
+
+
+
+
+
+
+
+
+
+
+
+
+
